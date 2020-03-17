dad0c963158a39c6bd4cd42d571cbf0a2cc8d652
Technote <technote.space@gmail.com>
2020-03-18 00:49:18 +0900

    feat: support json (#5)

--- src/process.ts
+++ src/process.ts
@@ -4,20 +4,14 @@ import { Logger } from '@technote-space/github-action-helper';
 import { getConfig } from '@technote-space/github-action-config-helper';
 import { getConfigFilename, getRelativePath, setEnv } from './utils/misc';
 
-export const execute = async(logger: Logger, octokit: Octokit, context: Context): Promise<boolean> => {
+export const execute = async(logger: Logger, octokit: Octokit, context: Context): Promise<void> => {
 	const config = await getConfig(getConfigFilename(), octokit, context, {configPath: getRelativePath()});
-	if (false === config) {
-		logger.warn('The specified file [%s] does not exist or is invalid.', getConfigFilename());
-		return false;
-	}
 
-	logger.startProcess('Target config: ');
+	logger.startProcess('Target config:');
 	console.log(config);
 	logger.endProcess();
 
 	Object.keys(config).forEach(key => {
 		setEnv(key, JSON.stringify(config[key]));
 	});
-
-	return true;
 };
