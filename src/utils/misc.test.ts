b7d9146c035b0dc2784c40a37c046b984b92a8f0
Technote <technote.space@gmail.com>
2022-06-01 23:47:51 +0900

    chore: tweaks

--- src/utils/misc.test.ts
+++ src/utils/misc.test.ts
@@ -4,8 +4,6 @@ import {
   spyOnExportVariable,
   exportVariableCalledWith,
 } from '@technote-space/github-action-test-helper';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { describe, expect, it } from 'vitest';
 import { getConfigFilenames, getRelativePath, setEnv, isIgnoreWarning } from './misc';
 
